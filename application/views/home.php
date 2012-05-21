eb9f458c5cb8229f60148a5e2e6dd13f07ca9cd0
Ward Peeters <warrepeeters@gmail.com>
2012-05-21 11:45:12 +0200

    javascript changes and some css stuff

--- application/views/home.php
+++ application/views/home.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
 <title>Equity</title>
 
 <link href="<?=base_url('css/styles.css');?>" rel="stylesheet" />
@@ -11,7 +12,7 @@
 <div id="container">
     <div id="main"><img src="<?=base_url('images/image01.jpg');?>" width="500" height="375" alt="" rel="1" /></div>
 
-    <div id="children"><img src="<?=base_url('images/image02.jpg');?>" width="500" height="375" alt="" rel="2" /> <img src="<?=base_url('images/image03.jpg');?>" width="500" height="375" alt="" rel="3" /> <img src="<?=base_url('images/image04.jpg');?>" width="500" height="375" alt="" rel="4" /></div>
+    <div id="children"><img src="<?=base_url('images/image02.jpg');?>" width="250" height="188" alt="" rel="2" /><img src="<?=base_url('images/image03.jpg');?>" width="250" height="188" alt="" rel="3" /><img src="<?=base_url('images/image04.jpg');?>" width="250" height="188" alt="" rel="4" /></div>
     <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
 </div>
 
@@ -21,9 +22,15 @@
         $main = $('#main'),
         $children = $('#children');
     $('img', '#children').click(function() {
+        updateMainImage($(this));
+    });
+
+    function updateMainImage($this) {
         $mImg = $('img', $main);
-        $img = $(this).clone()
+        $img = $this.clone()
             .css({'z-index': 5})
+            .width(500)
+            .height(375)
             .hide();
         $main.append($img);
 
@@ -32,21 +39,43 @@
             $mImg.remove();
         });
 
-        $.get('<?=site_url('home/next');?>', 'category=' + $(this).attr('rel'), function(data) {
+        $.get('<?=site_url('home/next');?>', 'category=' + $this.attr('rel'), function(data) {
             if (data.next) {
                 createNewImages(data.data);
             } else {
                 createInfoPage(data.data);
             }
         }, 'json');
-    });
+    }
 
     function createInfoPage(data) {
+        $('img', $children).remove();
+        $children.addClass('text');
         
+        $children.html(data.content);   
     }
     
     function createNewImages(data) {
-        alert(data);
+        var $images = $('img', $children);
+        for(var i=0; i<data.length; i++) {
+            var $img = $(document.createElement('img'))
+                .attr('src', data[i].image)
+                .attr('rel', data[i].id)
+                .attr('alt', data[i].title)
+                .width(250)
+                .height(188);
+            $img.click(function() {
+                updateMainImage($(this));
+            });
+
+            $images.eq(i).remove();
+            $children.append($img);
+        }
+
+        $images = $('img', $children);
+        for(var j=0; j<$images.length - i; j++) {
+            $images.eq(j).remove();
+        }
     }
 });</script>
 </body>
